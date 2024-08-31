37c4d03df3e9423867b39c4cd37516ceb8cac195
Azmankhan <azmankhan756@gmail.com>
2024-08-31 17:27:33 +0530

    uploaded all final file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="AzmanKhan",
+    author_email="Azmankhan756@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
